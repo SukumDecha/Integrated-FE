22c0d84d41008db49d28543935da7319db353467
SukumDecha <ninemaster12gt@gmail.com>
2025-05-05 23:38:24 +0700

    [038] add Dockerfile

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:5173',
-    baseAPI: 'http://localhost:8080',
+    baseUrl: 'http://ip24ssa1.sit.kmutt.ac.th',
+    baseAPI: 'http://ip24ssa1.sit.kmutt.ac.th/itb-mshop',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
