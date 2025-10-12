857a06312599e3173c69119580ab1c32123ca140
Chicha Aksawasewe <chichacute28@gmail.com>
2025-10-12 21:27:33 +0700

    [024] fix(order) : correct URL constants for order and seller order retrieval

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,14 +1,14 @@
 import { get, post } from './helper/api'
 import { buildSearchParams } from '@/utils'
 
-const BASE_URL_V2 = '/v2/users'
-const BASE_URL_V3 = '/v2/orders'
-const  BASE_ORDER_URL_V2 = '/v2/sellers'
+const BASE_USER_URL_V2 = '/v2/users'
+const BASE_ORDER_URL_V2 = '/v2/orders'
+const BASE_SALE_ORDERS_V2 = '/v2/sellers'
 
 const OrderService = {
   async getOrderByUserId(id, paginationParams = {}) {
     const queryString = buildSearchParams(paginationParams)
-    const url = `${BASE_URL_V2}/${id}/orders?${queryString}`
+    const url = `${BASE_USER_URL_V2}/${id}/orders?${queryString}`
 
     return await get(url, {
       isPaginated: true,
@@ -16,23 +16,23 @@ const OrderService = {
   },
 
   async placeOrder(payload) {
-    return await post(`${BASE_URL_V3}`, payload)
+    return await post(`${BASE_ORDER_URL_V2}`, payload)
   },
 
   async getOrderById(id) {
-    return await get(`${BASE_URL_V3}/${id}`)
+    return await get(`${BASE_ORDER_URL_V2}/${id}`)
   },
 
   // ---------- Seller Section ----------
 
     // ดึงรายการออเดอร์ของสินค้าของ seller
  async getOrdersBySellerId(sellerId) {
-  return await get(`${BASE_ORDER_URL_V2}/${sellerId}/orders`, { isPaginated: true })
+  return await get(`${BASE_SALE_ORDERS_V2}/${sellerId}/orders`, { isPaginated: true })
 },
 
   // ดึงรายละเอียดออเดอร์ (seller ดูได้)
   async getOrderBySeller(orderId) {
-    return await get(`${BASE_ORDER_URL_V2}/${orderId}`)
+    return await get(`${BASE_SALE_ORDERS_V2}/${orderId}`)
   },
 
 
